4cad8437c4b1c85fb00234bc8a911889f0995bdd
CarolineNakazato <carolinenakazato@yahoo.com.br>
2018-11-01 11:37:11 -0300

    Exclusão e parte da inserção

--- Projeto2.cpp
+++ Projeto2.cpp
@@ -1,4 +1,4 @@
-#define _CRT_SECURE_NO_WARNINGS
+Ôªø#define _CRT_SECURE_NO_WARNINGS
 #define MAX 10
 #include <stdio.h>
 #include <stdlib.h> 
@@ -16,22 +16,28 @@ enum menuAltera
 typedef struct {
 	int codigo;
 	char nome[20];
-	char categoria[20]; //suspense, romance, terror
+	char categoria[10]; //suspense, romance, terror, comedia
 	float preco;
-	float classificacao; //nota da crÌtica
+	float classificacao; //nota da crÔøΩtica
 }filme;
 
 
-int buscaBinaria (int vet[], int chave);
-filme leArquivo(int posicao, int chave);
+int buscaBinaria(int vet[], int chave);
+int leArquivo(filme filmes[]);
 void imprime(filme filmeAtual);
+int exclusao(filme filmes[], int posicao, int qtd);
+int insere(filme filmeAtual, int qtd, filme filmes[]);
 
 void main() {
 	filme filmes[MAX];
 	filme filmeAtual;
-	int opcao, opcaoAlterar, codAtual, posicao;
+	int opcao, opcaoAlterar, qtd = -1, codAtual, posicao;
 	int index[MAX];
-
+	char novoNome[20], novaCategoria[10];
+	double novoPreco, novaNota;
+	//inicializa vetor
+	qtd = leArquivo(filmes);
+	
 	do {
 		printf("Digite:\n");
 		printf("%d) Consultar filme.\n", consultar);
@@ -45,41 +51,36 @@ void main() {
 		switch (opcao)
 		{
 		case consultar:
-			//Entrada da chave prim·ria;
+			//Entrada da chave primÔøΩria;
 			printf("Digite o codigo do filme:\n");
 			scanf("%d", &codAtual);
 
-			//Carregar Õndice denso (item a);
+			//Carregar ÔøΩndice denso (item a);
 
-			//Chamar a funÁ„o Busca Bin·ria para localizar a chave;
+			//Chamar a funÔøΩÔøΩo Busca BinÔøΩria para localizar a chave;
 			posicao = buscaBinaria(index, codAtual);
 
 			//Caso exista o valor buscado;
-			if(posicao!=-1){
-				//Le no arquivo o registro procurado;
-				filmeAtual = leArquivo(posicao, codAtual);
-				//Imprimir todas as informaÁıes do registro buscado;
-				imprime(filmeAtual);
-				
-			}else{
+			if (posicao != -1) {
+				//Imprimir todas as informaÔøΩÔøΩes do registro buscado;
+				imprime(filmes[posicao]);
+
+			}
+			else {
 				printf("Codigo do filme nao existe.\n");
 			}
 			break;
 		case alterar:
-			//Entrada da chave prim·ria;
+			//Entrada da chave primÔøΩria;
 			printf("Digite o codigo do filme:\n");
 			scanf("%d", &codAtual);
 
-			//Carregar Õndice denso (item a);
+			//Carregar ÔøΩndice denso (item a);
 
-			//Chamar a funÁ„o Busca Bin·ria para localizar a chave;
+			//Chamar a funÔøΩÔøΩo Busca BinÔøΩria para localizar a chave;
 			posicao = buscaBinaria(index, codAtual);
-
-			//Caso exista o valor buscado;
-			if(posicao!=-1){
-				//Le no arquivo o registro procurado;
-				filmeAtual = leArquivo(posicao, codAtual);
-				//Perguntar qual campo ser· modificado, um a um;
+			if (posicao != -1) {
+				//Perguntar qual campo serÔøΩ modificado, um a um;
 				printf("Digite:\n");
 				printf("%d) para alterar o nome filme.\n", alteraNome);
 				printf("%d) para alterar a categoria do filme.\n", alteraCategoria);
@@ -87,31 +88,86 @@ void main() {
 				printf("%d) para alterar nota do filme.\n", alteraClassificacao);
 				printf("%d) Sair.\n", alteraSair);
 				scanf("%d", &opcaoAlterar);
-				//Alterar o(s) campo(s) no arquivo;
-				//Caso a alteraÁ„o ocorra no campo 2 e/ou no da categoria (campo3) ñ atualizar os respectivos Õndices; 
-				switch (opcaoAlterar){
+				
+				switch (opcaoAlterar) {
 				case alteraNome:
-						break;
+					printf("Digite o nome do filme:\n");
+					scanf("%s", novoNome);
+					strcpy(filmes[posicao].nome,novoNome);
+					//atualiza indice 
+					break;
 				case alteraCategoria:
-						break;
+					printf("Digite a categoria do filme:\n");
+					scanf("%s", novaCategoria);
+					strcpy(filmes[posicao].nome, novaCategoria);
+					//atualiza indice 
+					break;
 				case alteraPreco:
-						break;
+					printf("Digite o preco do filme:\n");
+					scanf("%f", &novoPreco);
+					filmes[posicao].preco = novoPreco;
+					break;
 				case alteraClassificacao:
-						break;
+					printf("Digite a nota do filme:\n");
+					scanf("%f", &novaNota);
+					filmes[posicao].classificacao = novaNota;
+					break;
 				case alteraSair:
-						break;
+					break;
 				default:
 					printf("Opcao Invalida!\n");
 					break;
 				} while (opcao != alteraSair);
-			
-			}else{
+				//atualizar arquivo???
+			}
+			else {
 				printf("Codigo do filme nao existe.\n");
 			}
 			break;
-		case excluir:
-			break;
 		case inserir:
+			do {
+				//Entrada da chave primÔøΩria;
+				printf("Digite o codigo do filme:\n");
+				scanf("%d", &filmeAtual.codigo);
+
+				//Verifica se o codigo do filmes ja existe
+				posicao = buscaBinaria(index, filmeAtual.codigo);
+				
+				if (posicao == -1) {
+					printf("Digite o nome do filme:\n");
+					scanf("%s", filmeAtual.nome);
+					printf("Digite a categoria do filme:\n");
+					scanf("%s", filmeAtual.categoria);
+					//falta classificacao e preco
+					qtd = insere(filmeAtual, qtd, filmes);
+					printf("Insercao feita com sucesso!\n");
+					//Atualizar os respectivos √çndices;
+				}
+				else {
+					printf("Codigo do filme ja existe.\n");
+				}
+			} while (posicao!=-1);
+			break;
+		case excluir:
+			//Entrada da chave primÔøΩria;
+			printf("Digite o codigo do filme:\n");
+			scanf("%d", &codAtual);
+
+			//Carregar ÔøΩndice denso (item a);
+
+			//Chamar a funÔøΩÔøΩo Busca BinÔøΩria para localizar a chave;
+			posicao = buscaBinaria(index, codAtual);
+
+			//Caso exista o valor buscado;
+			if (posicao != -1) {
+				//Inserir o registro no final do vetor;
+				qtd = exclusao(filmes, posicao, qtd);
+				//Atualizar os respectivos √çndices;
+
+			}
+			else {
+				printf("Codigo do filme nao existe.\n");
+			}
 			break;
 		case imprimir:
 			break;
@@ -126,50 +182,66 @@ void main() {
 
 }
 
-//retorna posicao da chave no vetor ou -1 caso n„o existe no vetor
-int buscaBinaria (int vet[], int chave){
-     int inf = 0, sup = MAX-1, meio;
-     while (inf <= sup){
-          meio = (inf + sup)/2;
-          if (chave == vet[meio])
-               return meio;
-          if (chave < vet[meio])
-               sup = meio-1;
-          else
-               inf = meio+1;
-     }
-     return -1;   // n„o encontrado
+//retorna posicao da chave no vetor ou -1 caso nÔøΩo existe no vetor
+int buscaBinaria(int vet[], int chave) {
+	int inf = 0, sup = MAX - 1, meio;
+	while (inf <= sup) {
+		meio = (inf + sup) / 2;
+		if (chave == vet[meio])
+			return meio;
+		if (chave < vet[meio])
+			sup = meio - 1;
+		else
+			inf = meio + 1;
+	}
+	return -1;   // nÔøΩo encontrado
 }
 
-//retorna do arquivo um filme especifico
-filme leArquivo(int posicao, int chave){
+//retorna a quantidade de filmes de um arquivo e popula o vetor de filmes
+int leArquivo(filme filmes[]){
 	FILE *arq;
-	filme filmes[MAX], filmeAtual;
-	int i, qtd;
+	filme filmeAtual;
+	int i, qtd = -1;//n√£o existe nenhum filme
 
-	// Abre um arquivo BIN¡RIO para LEITURA
+	// Abre um arquivo BINÔøΩRIO para LEITURA
 	arq = fopen("arq.dat", "rb");
-	if (arq == NULL){
+	if (arq == NULL) {
 		printf("Problemas na abertura do arquivo\n");
-		return filmeAtual;
+		return -1; 
 	}
 
-	qtd = fread(&filmes[0], sizeof(filme), 100, arq);
+	fseek(arq, 0, SEEK_SET);
+	while (!feof(arq)) {
+		qtd++;
+		fread(&filmes[qtd], sizeof(filme), 1, arq);
+	}
 
-	for (i=0; i<qtd; i++)
-		if(chave == filmes[i].codigo){
-			filmeAtual = filmes[i];
-		}
-
-	return filmeAtual;
 	fclose(arq);
+	return qtd;
+	
 }
 
-//Imprime as informaÁıes do filme;
-void imprime(filme filmeAtual){
-	printf("Codigo do filme: %d\n",filmeAtual.codigo);
-	printf("Nome do filme: %s\n",filmeAtual.nome);
-	printf("Tipo do filme: %s\n",filmeAtual.categoria);
-	printf("Preco do filme: %.2f\n",filmeAtual.preco);
-	printf("Nota da critica: %.2f\n",filmeAtual.classificacao);
+//Imprime as informaÔøΩÔøΩes do filme;
+void imprime(filme filmeAtual) {
+	printf("Codigo do filme: %d\n", filmeAtual.codigo);
+	printf("Nome do filme: %s\n", filmeAtual.nome);
+	printf("Tipo do filme: %s\n", filmeAtual.categoria);
+	printf("Preco do filme: %.2f\n", filmeAtual.preco);
+	printf("Nota da critica: %.2f\n", filmeAtual.classificacao);
+}
+
+//exclui um filme do vetor
+int exclusao(filme filmes[], int posicao, int qtd) {
+	int i;
+	for (i = posicao + 1; i < qtd; ++i)
+		filmes[i - 1] = filmes[i];
+	return qtd - 1;
+}
+
+//inserir filme no final do vetor
+int insere(filme filmeAtual, int qtd, filme filmes[])
+{
+	if(qtd+1<MAX-1)
+		filmes[qtd+1] = filmeAtual;
+	return qtd + 1;
 }
